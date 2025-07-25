5d09ee513ba46892c715d0ca4757c95e59f6b369
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-26 08:42:28 +0900

    初回コミット

---
+++ app/models/QRCode.server.js
@@ -0,0 +1,34 @@
+import qrcode from "qrcode";
+import invariant from "tiny-invariant";
+import db from "../db.server";
+
+export async function getQRCode(id, graphql) {
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  if (!qrCode) {
+    return null;
+  }
+
+  return supplementQRCode(qrCode, graphql);
+}
+
+export async function getQRCodes(shop, graphql) {
+  const qrCodes = await db.qRCode.findMany({
+    where: { shop },
+    orderBy: { id: "desc" },
+  });
+
+  if (qrCodes.length === 0) return [];
+
+  return Promise.all(
+    qrCodes.map((qrCode) => supplementQRCode(qrCode, graphql)),
+  );
+}
+
+// 「QR コード画像を取得する」ライブラリを使用して、base64エンコード
+export function getQRCodeImage(id) {
+  const url = new URL(`/qrcodes/${id}/scan`, process.env.SHOPIFY_APP_URL);
+  return qrcode.toDataURL(url.href);
+}
+
+
