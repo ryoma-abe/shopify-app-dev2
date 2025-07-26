084ddd7e9212d63afc0f6888832e948c30b10f5e
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-26 09:27:42 +0900

    QRコードの補足情報を取得する非同期関数を追加し、QRコードデータのバリデーション機能を実装

--- app/models/QRCode.server.js
+++ app/models/QRCode.server.js
@@ -31,14 +31,78 @@ export function getQRCodeImage(id) {
   return qrcode.toDataURL(url.href);
 }
 
-
 export function getDestinationUrl(qrCode) {
   if (qrCode.destination === "product") {
     return `https://${qrCode.shop}/products/${qrCode.productHandle}`;
   }
 
-  const match = /gid:\/\/shopify\/ProductVariant\/([0-9]+)/.exec(qrCode.productVariantId);
+  const match = /gid:\/\/shopify\/ProductVariant\/([0-9]+)/.exec(
+    qrCode.productVariantId,
+  );
   invariant(match, "Unrecognized product variant ID");
 
   return `https://${qrCode.shop}/cart/${match[1]}:1`;
+}
+
+async function supplementQRCode(qrCode, graphql) {
+  const qrCodeImagePromise = getQRCodeImage(qrCode.id);
+
+  const response = await graphql(
+    `
+      query supplementQRCode($id: ID!) {
+        product(id: $id) {
+          title
+          media(first: 1) {
+            nodes {
+              preview {
+                image {
+                  altText
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: {
+        id: qrCode.productId,
+      },
+    },
+  );
+
+  const {
+    data: { product },
+  } = await response.json();
+
+  return {
+    ...qrCode,
+    productDeleted: !product?.title,
+    productTitle: product?.title,
+    productImage: product?.media?.nodes[0]?.preview?.image?.url,
+    productAlt: product?.media?.nodes[0]?.preview?.image?.altText,
+    destinationUrl: getDestinationUrl(qrCode),
+    image: await qrCodeImagePromise,
+  };
+}
+
+export function validateQRCode(data) {
+  const errors = {};
+
+  if (!data.title) {
+    errors.title = "Title is required";
+  }
+
+  if (!data.productId) {
+    errors.productId = "Product is required";
+  }
+
+  if (!data.destination) {
+    errors.destination = "Destination is required";
+  }
+
+  if (Object.keys(errors).length) {
+    return errors;
+  }
 }
