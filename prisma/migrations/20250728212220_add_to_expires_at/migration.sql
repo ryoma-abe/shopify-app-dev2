bc9077cd0837b0fadabf3c84e05b56a9a18e34a5
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-29 06:25:59 +0900

    QRCodeモデルにexpiresAtフィールドを追加し、スキーマの整形を改善

---
+++ prisma/migrations/20250728212220_add_to_expires_at/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_QRCode" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "productHandle" TEXT NOT NULL,
+    "productVariantId" TEXT NOT NULL,
+    "destination" TEXT NOT NULL,
+    "scans" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_QRCode" ("createdAt", "destination", "id", "productHandle", "productId", "productVariantId", "scans", "shop", "title") SELECT "createdAt", "destination", "id", "productHandle", "productId", "productVariantId", "scans", "shop", "title" FROM "QRCode";
+DROP TABLE "QRCode";
+ALTER TABLE "new_QRCode" RENAME TO "QRCode";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
